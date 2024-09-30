c05fd583ee84f20b587c041d2e35f63488978629
Chesh1M <50264743+Chesh1M@users.noreply.github.com>
2024-10-01 01:34:17 +0800

    test env var

--- src/components/Qotd/Qotd.jsx
+++ src/components/Qotd/Qotd.jsx
@@ -4,8 +4,10 @@ import styles from "./Qotd.module.css";
 //import quotes from "./quotes";
 
 export const Qotd = () => {
-  const SPACE_ID = import.meta.env.VITE_SPACE_ID;
-  const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
+  // const SPACE_ID = import.meta.env.VITE_SPACE_ID;
+  // const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
+  const SPACE_ID = "ydwuiufq31h7";
+  const ACCESS_TOKEN = "uDy5EiEPBuWb8TQUnDk5fcaHNF1fKU1nzOPiGhZwk1E";
 
   // save quotes from contentful cms
   const [quotes, setQuotes] = useState([]);
